61bf0daf74a214b06a63de80f8baa494cbfca33e
DmitriyPetrenko <dmitriy.petrenko7@gmail.com>
2018-08-03 19:51:39 +0300

    webpack-template

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import printMe from './print.js';
+
+function component() {
+  var element = document.createElement('div');
+  var button = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  button.innerHTML = 'Click me and check the console!';
+  button.onclick = printMe;
+
+  element.appendChild(button);
+
+  return element;
+}
+
+document.body.appendChild(component());
