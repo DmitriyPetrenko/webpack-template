5df17b8d42143b44b5526f953b51d0e964c8218b
DmitriyPetrenko <dmitriy.petrenko7@gmail.com>
2018-10-08 19:59:37 +0300

    webpack config splitting

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -7,32 +7,14 @@ module.exports = {
     entry: {
         app: './src/index.js'
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    performance: {
-        hints: false
-    },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
-        }),
-        new webpack.HotModuleReplacementPlugin()
+        })
     ],
     output: {
         filename: '[name].bundle.js',
         path:     path.resolve(__dirname, 'dist'),
     },
-    mode: 'development'
 };
